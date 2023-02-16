3d1fb6379716497dd392f905bc26a1a6829a0b1c
ssakaibrahim23 <ssakaibrahim23@gmail.com>
2023-02-16 13:53:51 -0800

    4. Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A C program that give output using puts command
+ * Return: 0;
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
