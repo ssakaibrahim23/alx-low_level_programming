d91f6292cbfa71196c268ccc5f934de616dc314e
ssakaibrahim23 <ssakaibrahim23@gmail.com>
2023-02-16 14:44:03 -0800

    5. Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /**
  *main - C program that uses printf instead of puts
- * Return: 0;
+ * Return: 0
  */
-
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n")
-	return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n")
+return (0);
 }
