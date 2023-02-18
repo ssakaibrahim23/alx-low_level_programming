4ac71fc253910d97253f21bff8185a9d703ab875
ssakaibrahim23 <ssakaibrahim23@gmail.com>
2023-02-17 17:04:19 -0800

    1. The last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,9 +1,8 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-
 /**
- * main - This program will assign a random number to the variable n each time it is executed
+ * main - This program will assign a random number to the variable n
  * Rturn:Always  (Sucess)
  */
 
