e9688d998aca686d6d6eaf7f7af5efeea6ab439e
ssakaibrahim23 <ssakaibrahim23@gmail.com>
2023-02-18 15:57:29 -0800

    9. Patience, persistence and perspiration make an unbeatable combination for success

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all cmbinations of single-digit numbers
+ * in ascending order each separated by ',' followed by a space
+ * Return: 0
+ */
+
+int main(void)
+{
+	int no;
+
+	for (no = 48; no < 58; no++)
+	{
+		putchar(no);
+		if (no != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		putchar('\n');
+	return (0);
+}
