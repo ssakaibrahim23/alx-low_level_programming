359ec0c98310d95e610cbe4a55df78c7fb830a45
ssakaibrahim23 <ssakaibrahim23@gmail.com>
2023-02-17 18:55:51 -0800

    2. I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,31 +1,35 @@
 #include <stdlib.h>
+
 #include <time.h>
+
 #include <stdio.h>
+
+
 /**
  * main - This program will assign a random number to the variable n
- * Rturn:Always  (Sucess)
+ * Rturn: (0)
  */
-
 int main(void)
 
 {
-	int n, lstd
-		;
+
+	int n, lstd;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	lstd = n % 10;
 
+	lstd = n % 10;
 	if (lstd > 5)
 	{
-		printf("Last digit of %d is %d and is greater than 5\n", n, lstd);
+	printf("Last digit of %d is %d and is greater than 5\n", n, lstd);
 	}
 	else if (lstd == 0)
 	{
-		printf("Last digit of %d is %d and is 0\n", n, lstd);
+	printf("Last digit of %d is %d and is 0\n", n, lstd);
 	}
 	else if (lstd < 6 && lstd != 0)
 	{
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lstd);
+	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lstd);
 	}
 	return (0);
 }
---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio>
+
+/**
+ * main - Prints the alphabetic
+ * Return: Alwas (Sucess)
+ */
+
+int main(void)
+
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	putchar(c);
+	putchar('\n');
+	return (0);
+
+}
